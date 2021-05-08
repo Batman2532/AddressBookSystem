a98d76917ddce2ab78cd171e66e7cc93800c5c14
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-05-08 16:21:52 +0530

    Refactor: changes done in add method

--- src/com/company/AddressBook.java
+++ src/com/company/AddressBook.java
@@ -1,46 +1,50 @@
 package com.company;
-
-
 import java.util.*;
 
 public class AddressBook {
-    public static String firstName;
-    public static String lastName;
-    public static String address;
-    public static String city;
-    public static String state;
-    public static int zipCode;
-    public static int phoneNumber;
-    public static String email;
 
-    public AddressBook(String firstName, String lastName, String address,String city,String state,int zipCode,int phoneNumber,String email){
-        this.firstName = firstName;
-        this.lastName =lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zipCode = zipCode;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
+    private static ArrayList<Contacts> person = new ArrayList<Contacts>();
+    static Scanner sc = new Scanner(System.in);
+
+    public static void addPerson(){
+        String firstName,lastName,address,city,state,email;
+        int zipCode,phoneNumber;
+        System.out.println("Enter First Name");
+        firstName = sc.next();
+        System.out.println("Enter Last Name");
+        lastName = sc.next();
+        System.out.println("Enter Address");
+        address = sc.next();
+        System.out.println("Enter City");
+        city = sc.next();
+        System.out.println("Enter State");
+        state = sc.next();
+        System.out.println("Enter Zipcode");
+       zipCode = sc.nextInt();
+        System.out.println("Enter Phone Number");
+       phoneNumber = sc.nextInt();
+        System.out.println("Enter Email");
+       email = sc.next();
+        Contacts ab = new Contacts(firstName, lastName, address, city, state, zipCode, phoneNumber, email);
+        System.out.println(ab);
+        person.add(ab);
     }
 
     public static void main(String[] args){
       System.out.println("Welcome to Address Book System");
-      ArrayList<Contacts> contacts = new ArrayList<>();
-      Contacts ab = new Contacts(firstName, lastName, address, city, state, zipCode,phoneNumber,email);
         System.out.println("Enter 1 to add contact: ");
-        Scanner sc = new Scanner(System.in);
         int choice = sc.nextInt();
         switch(choice) {
             case 1:
-                ab.add();
-                ab.toString();
-                contacts.add(ab);
-                System.out.println(contacts.get(0));
+                addPerson();
                 break;
             default:
                 System.out.println("Enter valid number");
                 break;
         }
     }
+
 }
+//    String firstName, String lastName, String address, String city, String state, int zipCode, int phoneNumber, String email
+//firstName, lastName, address, city, state, zipCode,phoneNumber,email
+//String fName, String lName, String add, String cty, String st, int zip, int ph, String mail
--- src/com/company/Contacts.java
+++ src/com/company/Contacts.java
@@ -1,42 +1,67 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class Contacts{
 
-    public static String firstName;
-    public static String lastName;
-    public static String address;
-    public static String city;
-    public static String state;
-    public static int zipCode;
-    public static int phoneNumber;
-    public static String email;
+    String firstName,lastName,address,city,state,email;
+    int zipCode,phoneNumber;
     public  Contacts(String firstName, String lastName, String address, String city, String state, int zipCode, int phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName =lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
     }
-        public void add(){
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter First Name");
-            this.firstName = sc.nextLine();
-            System.out.println("Enter Last Name");
-            this.lastName = sc.nextLine();
-            System.out.println("Enter Address");
-            this.address = sc.nextLine();
-            System.out.println("Enter City");
-            this.city = sc.nextLine();
-            System.out.println("Enter State");
-            this.state = sc.nextLine();
-            System.out.println("Enter Zipcode");
-            this.zipCode = sc.nextInt();
-            System.out.println("Enter Phone Number");
-            this.phoneNumber = sc.nextInt();
-            System.out.println("Enter Email");
-            this.email = sc.next();
-        }
-        public String toString(){
-            return ("The entered details are: \nfirst name " +firstName+"\nlast name "+ lastName+ "\nAddress "+address+"\ncity "+city+"\nstate " +state+"\nzip code "+zipCode+"\nphone number "+phoneNumber+"\nemail "+email);
-
-        }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getAddress() {
+        return firstName;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public int getZipCode() {
+        return zipCode;
+    }
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmail() {
+        return email;
+    }
 
+}
